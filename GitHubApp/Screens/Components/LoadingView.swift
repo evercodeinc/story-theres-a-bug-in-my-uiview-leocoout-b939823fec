dfe300ebbaa715999b8e57332b2aabd5afc9ae2a
leonardo santos <leonardo.santos@moises.ai>
2023-02-06 18:16:44 -0300

    add startAnimation method

--- GitHubApp/Screens/Components/LoadingView.swift
+++ GitHubApp/Screens/Components/LoadingView.swift
@@ -6,6 +6,7 @@ final class LoadingView: UIView {
       
         let view = UIActivityIndicatorView(style: .large)
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.startAnimating()
         return view
     }()
     
